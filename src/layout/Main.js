473dbfd537fe1a9b49a78d1f77a2f95331ba4e6f
mahian ahmad <mahianahamd9@gmail.com>
2022-11-24 11:52:49 +0600

    add hero section, navbar, footer

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/shared/Footer';
+import Nav from '../components/shared/Nav';
 
 const Main = () => {
     return (
         <div>
-            nav
+            <Nav/>
             <Outlet/>
-            footer
+            <Footer/>
         </div>
     );
 };
---
+++ src/utilitys/SectionTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionTitle = ({title}) => {
+    return (
+        <div>
+            <h2 className='text-primary font-semibold text-2xl'>{title}</h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
