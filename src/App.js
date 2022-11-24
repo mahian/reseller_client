b667cd4c3986fa29e78ced656e65eed40218dd61
mahian ahmad <mahianahamd9@gmail.com>
2022-11-24 11:19:42 +0600

    setup router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main />,
+      children: [
+        {path: '/', element: <Home/>},
+        {path: 'login', element: <Login/>},
+        {path: 'signup', element: <Signup/>},
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/config/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAo3ZrhwJFI36TO0G2YZUeccslVMnb5Mlw",
+  authDomain: "reseller-a8ff7.firebaseapp.com",
+  projectId: "reseller-a8ff7",
+  storageBucket: "reseller-a8ff7.appspot.com",
+  messagingSenderId: "595974908837",
+  appId: "1:595974908837:web:3ffe17ac3b66122838564c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
