85fb119f847a80bac2b57144351c170dea8f10c9
mahian ahmad <mahianahamd9@gmail.com>
2022-11-29 20:13:12 +0600

    fix product

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -4,19 +4,19 @@ const Product = ({ product, setProduct }) => {
     const { title, price, condition, image, date, userName, userImg, original_price } = product;
     return (
         <div className="flex justify-center">
-            <div className="rounded-lg shadow-lg bg-white">
+            <div className="rounded-lg shadow-lg bg-white w-full">
                 <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
                     <img className="rounded-t-lg h-60 w-full object-cover" src={image} alt="" />
                 </a>
                 <div className="p-6">
                     <h5 className="text-gray-900 text-xl font-bold mb-2">{title}</h5>
                     <div className='flex items-center'>
-                        <h3 className="text-primary font-bold mr-3 text-3xl">{original_price}৳</h3>
-                        <p className="text-gray-500 font-semibold relative before:content-[''] before:w-full before:h-[1px] before:bg-gray-400 before:absolute before:top-[13px]">{price}৳</p>
+                        <h3 className="text-primary font-bold mr-3 text-3xl">{price}৳</h3>
+                        <p className="text-gray-500 font-semibold relative before:content-[''] before:w-full before:h-[1px] before:bg-gray-400 before:absolute before:top-[13px]">{original_price}৳</p>
                     </div>
                     <p className="text-gray-700 font-semibold mt-2"><strong>condition : </strong>{condition}</p>
                     <div className='flex items-center mt-4 mb-7'>
-                        <img className='h-10 w-10 rounded-full' src={userImg} alt="" />
+                        <img className='h-10 w-10 rounded-full object-cover' src={userImg} alt="" />
                         <div className='ml-3'>
                             <p className="text-gray-700 font-bold">{userName}</p>
                             <p className="text-gray-700 text-xs font-semibold">{date}</p>
